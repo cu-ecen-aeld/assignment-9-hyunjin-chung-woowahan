928ea80c885fd437634f491221e6b251c535faa5
hyunjin <hyunjin.chung@woowahan.com>
2023-09-22 12:31:31 +0900

    change run_qemu.sh, aesd-assignments package, aesdsocket-start-stop script

--- server/aesdsocket-start-stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR_PATH=$(dirname $(readlink -f "${BASH_SOURCE:-$0}"))
-
-start-stop-daemon --start --exec "${DIR_PATH}"/aesdsocket -- -d 
-if (( $? == 1 )); then
-  start-stop-daemon --stop --exec "${DIR_PATH}"/aesdsocket 
-fi
