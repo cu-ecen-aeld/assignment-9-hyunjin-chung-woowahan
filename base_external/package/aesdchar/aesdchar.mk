9d6770a6a7cf65569a1a208389609642ef23eec8
hyunjin <hyunjin.chung@woowahan.com>
2023-10-16 01:03:24 +0900

    complete assignment 8

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -4,7 +4,7 @@
 #
 ##############################################################
 
-AESDCHAR_VERSION = 2b83cb7d83f9f3570d397f2debb05e37d12f3bf0
+AESDCHAR_VERSION = 3e990ccd1a5a30fc1f059337c3a924ffb74c7116
 AESDCHAR_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-hyunjin-chung-woowahan.git
 AESDCHAR_SITE_METHOD = git
 AESDCHAR_GIT_SUBMODULES = YES
